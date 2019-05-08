a736f6df677c4122bb075baedbc76cc77ee8e7c5
Balaji Varadarajan <varadarb@uber.com>
2019-05-08 14:01:44 -0700

    Fix hoodie-cli

--- hoodie-cli/hoodie-cli.sh
+++ hoodie-cli/hoodie-cli.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-HOODIE_JAR=`ls $DIR/target/hoodie-cli-*-SNAPSHOT.jar`
+HOODIE_CLI_JAR=`ls $DIR/target/hoodie-cli-*.jar`
+DEP_JAR=`ls $DIR/lib/dnl/utils/textutils/0.3.3/textutils-0.3.3.jar`
+HOODIE_UTILITIES_JAR=`ls $DIR/hoodie-utilities/target/hoodie-utilities-*.jar`
 if [ -z "$HADOOP_CONF_DIR" ]; then
   echo "setting hadoop conf dir"
   HADOOP_CONF_DIR="/etc/hadoop/conf"
@@ -13,4 +15,4 @@ fi
 if [ -z "$CLIENT_JAR" ]; then
   echo "client jar location not set"
 fi
-java -cp ${HADOOP_CONF_DIR}:${SPARK_CONF_DIR}:$DIR/target/lib/*:$HOODIE_JAR:${CLIENT_JAR} -DSPARK_CONF_DIR=${SPARK_CONF_DIR} -DHADOOP_CONF_DIR=${HADOOP_CONF_DIR} org.springframework.shell.Bootstrap
+java -cp ${HADOOP_CONF_DIR}:${SPARK_CONF_DIR}:$DEP_JAR:$SPARK_HOME/jars/*:$HOODIE_CLI_JAR:$HOODIE_UTILITIES_JAR:${CLIENT_JAR} -DSPARK_CONF_DIR=${SPARK_CONF_DIR} -DHADOOP_CONF_DIR=${HADOOP_CONF_DIR} org.springframework.shell.Bootstrap
